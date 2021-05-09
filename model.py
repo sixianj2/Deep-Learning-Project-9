c74b9b02770c574f043eca3833d574d537f33936
sixianj2 <71730024+sixianj2@users.noreply.github.com>
2021-05-09 18:23:22 -0400

    Update model.py

--- model.py
+++ model.py
@@ -334,6 +334,7 @@ class CGAN_model():
 
         fig = plt.figure()
         plt.plot(self.epoch_x[0:-1], self.gan_losses)
+        plt.ylim([0, 1.8])
         plt.xlabel("epoch")
         plt.ylabel("GAN loss")
         plt.title("GAN loss")
