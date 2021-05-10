43868130b969848e1b4cdf51ddf4b2b279ccf8ba
sixianj2 <71730024+sixianj2@users.noreply.github.com>
2021-05-09 22:01:44 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-# Copyright 2020 Lorna Authors. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 import argparse
 import os
 import random
@@ -22,49 +9,35 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from tqdm import tqdm
 
-from cyclegan_pytorch import Generator
+# from cyclegan_pytorch import Generator
 from cyclegan_pytorch import ImageDataset
+from model import CGAN_model
+dataroot = "./data"
+dataset_p = "prof2drawing"
+image_size = 256
+outf_p = "./results"
+cuda = True
 
-parser = argparse.ArgumentParser(
-    description="PyTorch implements `Unpaired Image-to-Image Translation using Cycle-Consistent Adversarial Networks`")
-parser.add_argument("--dataroot", type=str, default="./data",
-                    help="path to datasets. (default:./data)")
-parser.add_argument("--dataset", type=str, default="horse2zebra",
-                    help="dataset name. (default:`horse2zebra`)"
-                         "Option: [apple2orange, summer2winter_yosemite, horse2zebra, monet2photo, "
-                         "cezanne2photo, ukiyoe2photo, vangogh2photo, maps, facades, selfie2anime, "
-                         "iphone2dslr_flower, ae_photos, ]")
-parser.add_argument("--cuda", action="store_true", help="Enables cuda")
-parser.add_argument("--outf", default="./results",
-                    help="folder to output images. (default: `./results`).")
-parser.add_argument("--image-size", type=int, default=256,
-                    help="size of the data crop (squared assumed). (default:256)")
-parser.add_argument("--manualSeed", type=int,
-                    help="Seed for initializing training. (default:none)")
-
-args = parser.parse_args()
-print(args)
 
 try:
-    os.makedirs(args.outf)
+    os.makedirs(outf_p)
 except OSError:
     pass
 
-if args.manualSeed is None:
-    args.manualSeed = random.randint(1, 10000)
-print("Random Seed: ", args.manualSeed)
-random.seed(args.manualSeed)
-torch.manual_seed(args.manualSeed)
+manualSeed = random.randint(1, 10000)
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
 
 cudnn.benchmark = True
 
-if torch.cuda.is_available() and not args.cuda:
+if torch.cuda.is_available() and not cuda:
     print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
 # Dataset
-dataset = ImageDataset(root=os.path.join(args.dataroot, args.dataset),
+dataset = ImageDataset(root=os.path.join(dataroot, dataset_p),
                        transform=transforms.Compose([
-                           transforms.Resize(args.image_size),
+                           transforms.Resize(image_size),
                            transforms.ToTensor(),
                            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
                        ]),
@@ -73,20 +46,26 @@ dataset = ImageDataset(root=os.path.join(args.dataroot, args.dataset),
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
 
 try:
-    os.makedirs(os.path.join(args.outf, str(args.dataset), "A"))
-    os.makedirs(os.path.join(args.outf, str(args.dataset), "B"))
+    os.makedirs(os.path.join(outf_p, str(dataset_p), "A"))
+    os.makedirs(os.path.join(outf_p, str(dataset_p), "B"))
 except OSError:
     pass
 
-device = torch.device("cuda:0" if args.cuda else "cpu")
+device = torch.device("cuda:0" if cuda else "cpu")
 
 # create model
-netG_A2B = Generator().to(device)
-netG_B2A = Generator().to(device)
+# netG_A2B = Generator().to(device)
+# netG_B2A = Generator().to(device)
+cgan_model = CGAN_model(100, 200, 0.0002, 1)
+netG_A2B = cgan_model.netG_A2B
+netG_B2A = cgan_model.netG_B2A
+
 
 # Load state dicts
-netG_A2B.load_state_dict(torch.load(os.path.join("weights", str(args.dataset), "netG_A2B.pth")))
-netG_B2A.load_state_dict(torch.load(os.path.join("weights", str(args.dataset), "netG_B2A.pth")))
+# netG_A2B.load_state_dict(torch.load(os.path.join("weights", str(dataset_p), "netG_A2B.pth")))
+# netG_B2A.load_state_dict(torch.load(os.path.join("weights", str(dataset_p), "netG_B2A.pth")))
+netG_A2B.load_state_dict(torch.load("weights/prof2drawing/netG_A2B.pth"))
+netG_B2A.load_state_dict(torch.load("weights/prof2drawing/netG_B2A.pth"))
 
 # Set model mode
 netG_A2B.eval()
@@ -100,11 +79,11 @@ for i, data in progress_bar:
     real_images_B = data["B"].to(device)
 
     # Generate output
-    fake_image_A = 0.5 * (netG_B2A(real_images_B).data + 1.0)
-    fake_image_B = 0.5 * (netG_A2B(real_images_A).data + 1.0)
+    fake_image_A = 0.5 * (cgan_model.netG_B2A(real_images_B).data + 1.0)
+    fake_image_B = 0.5 * (cgan_model.netG_A2B(real_images_A).data + 1.0)
 
     # Save image files
-    vutils.save_image(fake_image_A.detach(), f"{args.outf}/{args.dataset}/A/{i + 1:04d}.png", normalize=True)
-    vutils.save_image(fake_image_B.detach(), f"{args.outf}/{args.dataset}/B/{i + 1:04d}.png", normalize=True)
+    vutils.save_image(fake_image_A.detach(), f"{outf_p}/{dataset_p}/A/{i + 1:04d}.png", normalize=True)
+    vutils.save_image(fake_image_B.detach(), f"{outf_p}/{dataset_p}/B/{i + 1:04d}.png", normalize=True)
 
     progress_bar.set_description(f"Process images {i + 1} of {len(dataloader)}")
